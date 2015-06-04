34ffb64889060a14cbbf4d889fcf3eb8f615a350
unknown <trevorjbaron@gmail.com>
2015-06-04 03:17:11 -0400

    using jade

--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
-import test from "./libs/test"
+/// <reference path='typings/express/express.d.ts' />
+/// <reference path='typings/body-parser/body-parser.d.ts' />
 
-test("hi")
+import appFactory from "./libs/appFactory"
+
+var app = appFactory.createApp()
+
+app.get('/', function(req, res) {
+	res.render('index');    
+})
+
+app.listen(3000, function(){
+    console.log("Server running");
+});
---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="express/express.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
