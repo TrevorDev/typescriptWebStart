e9ac18c5633ae7cf547c56e9292e1f28f5013785
unknown <trevorjbaron@gmail.com>
2015-06-05 14:59:18 -0400

    browserify test

---
+++ public/ts/index.ts
@@ -0,0 +1,11 @@
+/// <reference path='../../typings/jquery/jquery.d.ts' />
+//import rivets from "rivets"
+import $ = require("jquery")
+import rivets = require("rivets")
+
+
+var t = {}
+
+rivets.bind()
+//$("body").html("gfdg")
+//console.log(rivets)
