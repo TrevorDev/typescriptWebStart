d8c5f7c1ffd8b30083629fc95483c5a2c1103f92
unknown <trevorjbaron@gmail.com>
2015-06-11 21:18:14 -0400

    removing folder to be ingmored

--- typings/tsd.d.ts
+++
@@ -1,7 +0,0 @@
-
-/// <reference path="express/express.d.ts" />
-/// <reference path="body-parser/body-parser.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="bootstrap/bootstrap.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="gulp/gulp.d.ts" />
