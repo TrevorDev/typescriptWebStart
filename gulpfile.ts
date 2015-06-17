cc3945ecf0e38d50d82fedc8b43401ccdb6e516d
unknown <trevorjbaron@gmail.com>
2015-06-16 23:07:01 -0400

    watching ts files

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,13 @@
 /// <reference path='typings/node/node.d.ts' />
 /// <reference path='typings/gulp/gulp.d.ts' />
-
+/// <reference path='typings/watch/watch.d.ts' />
+/// <reference path='typings/glob/glob.d.ts' />
 import gulp = require('gulp')
+import watch = require('watch')
 import cp = require('child_process')
+import glob = require('glob')
 
 var exec = cp.exec
-
 gulp.task('default', function() {
   exec("tsc", function(err, out, stderr){
     console.log(out, stderr)
@@ -14,5 +16,56 @@ gulp.task('default', function() {
 		  console.log("\nBuild Complete")
 	  })
   })
-  
 });
+
+gulp.task("watchTS",function(){
+  // glob("views/**", function(err, f){
+  //   console.log(f)
+  // })
+  watchFolder('./', function(file){
+    if(isTSFile(file)){
+      compileTSFile(file)
+    }
+  })
+})
+
+gulp.task("runAndWatch",function(){
+  
+  runCommandLine("nodemon app.js")
+  runCommandLine("gulp watchTS")
+})
+
+function isTSFile(file) {
+  return file.match(/[^.]*\.ts$/)
+}
+
+
+function runCommandLine(cmd){
+  var com = exec(cmd)
+  com.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+  
+  com.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+}
+
+function compileTSFile(file) {
+  console.log("Compiling ts: "+file)
+  exec("tsc "+file)
+}
+
+function watchFolder(folder:string, handler:Function) {
+  watch.createMonitor(folder, function (monitor) {
+    monitor.on("created", function (f, stat) {
+      handler(f)
+    })
+    monitor.on("changed", function (f, curr, prev) {
+      handler(f)
+    })
+    monitor.on("removed", function (f, stat) {
+      handler(f)
+    })
+  })
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+To install dependencies:
+	npm install tsd -g
+	npm install gulp -g
+	npm install bower -g
+	npm install typescript -g
+	npm install
+	tsd reinstall
+
+TO build then run:
+	gulp
+	node app.js
