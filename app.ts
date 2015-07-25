d2de3b5dd183b3a3f44c2a7d95733f38f22a1f06
trevorjbaron@gmail.com <trevorjbaron@gmail.com>
2015-07-25 13:32:54 -0700

    database start

--- app.ts
+++ app.ts
@@ -1,5 +1,22 @@
 import appFactory = require("./libs/appFactory")
 import browserify = require("browserify")
+import Sequelize = require('sequelize');
+let db = new Sequelize("postgres", "postgres", "", {
+	host: 'trevorb.cloudapp.net',
+  dialect: 'postgres'
+});
+let User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+db.sync({force: true})
 
 let app = appFactory.createApp()
 
